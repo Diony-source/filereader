e06dbe7b2e771e7fd5cbe009f7be0caed9f3210c
Diony-source <erendalkiran2001@gmail.com>
2024-12-18 22:57:29 +0300

    feat(handlers): Added system-wide file search and content display support.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,22 +1,44 @@
+// Manages user interaction for file reading
 package handlers
 
 import (
 	"filereader/services"
 	"fmt"
+	"runtime"
 )
 
 func Start() {
 	var filename string
 
-	fmt.Println("Enter the file name to read: ")
+	// Get file name from the user
+	fmt.Print("Enter the file name to search: ")
 	fmt.Scanln(&filename)
 
-	content, err := services.ReadFileContent(filename)
+	// Determine the root directory to start search
+	rootDir := "/"
+	if runtime.GOOS == "windows" {
+		rootDir = "C:\\" // Root directory for Windows
+	}
+
+	fmt.Println("Searching for the file...")
+
+	// Search for the file
+	foundPath, err := services.FindFileInSystem(rootDir, filename)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	fmt.Println("\nFile content:")
+	fmt.Println("File found at:", foundPath)
+
+	// Read file content
+	content, err := services.ReadFileContent(foundPath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// Display the file content
+	fmt.Println("\nFile Content:")
 	fmt.Println(content)
 }
